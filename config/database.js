fa5c54afac861ff6789ff80f2eae37eb9ad55923
encry973r <encry973r@gmail.com>
2017-12-11 11:59:38 +0100

    final testing
    final testing

--- config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	db: "mongodb://encry973r:calculus20@ds119436.mlab.com:19436/p2pdb",
-	mdb: "mongodb://localhost:27017/p2pdb",
-	secret: 'encry973r',
-	euser: 'admin@swaaypay.com',
-	epass: 'Calculus20'
-	
-}
